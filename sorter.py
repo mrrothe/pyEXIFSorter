bb4c56f1c80317785c3aca23030a63c5a6182b0e
Martin Rothe <martin@rothe.uk>
2021-08-07 13:35:03 +0100

    Initial commit

---
+++ sorter.py
@@ -0,0 +1,26 @@
+from exif import Image
+import os
+import datetime
+
+directory = r'C:\Users\MartinRothe\OneDrive - Rothe\Pictures\Joscha&Ruth\Picked'
+# directory = r'/mnt/c/Users/MartinRothe/OneDrive - Rothe/Pictures/Joscha&Ruth/Picked'
+
+
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg"):
+        with open(os.path.join(directory, filename), "rb") as image_file:
+            my_image = Image(image_file)
+        image_taken= datetime.datetime.strptime(my_image.datetime_original, '%Y:%m:%d %H:%M:%S')
+        if "canon" in (my_image.make).lower():
+            newfilename = f'c:\\temp\photos\canon\{image_taken.year}\{image_taken.month}\{filename}'
+        else:
+            newfilename = f'c:\\temp\photos\{image_taken.year}\{image_taken.month}\{filename}'
+        if os.path.isfile(newfilename):
+            print("File already Exists: " + newfilename)
+        else:
+            print("Moving " + os.path.join(directory, filename) + " to " + newfilename)
+#            os.path.join(*filename.split("\\")[:-1])
+#            os.makedirs(os.path.dirname(newfilename)    , exist_ok=True)
+#            os.rename(os.path.join(directory, filename), newfilename)
+    else:
+        continue
